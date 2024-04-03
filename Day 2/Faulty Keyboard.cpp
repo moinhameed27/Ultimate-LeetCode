8f415346ce852fbd519ee4fe48f633892d67ffc2
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-03 09:38:54 +0500

    Faulty Keyboard

---
+++ Day 2/Faulty Keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans;
+        for(int i = 0; i < s.size(); i++)
+        {
+            // If found 'i' reverse the string otherwise add to answer
+            if(s[i] != 'i')
+                ans += s[i];
+            else
+                reverse(ans.begin(), ans.end());
+        }
+        return ans;
+    }
+};
