2f0e728a2c4d3196d55ef412b7ac4470d25cc6a6
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-03 09:36:58 +0500

    Long Pressed Name

---
+++ Day 1/Long Pressed Name.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    bool isLongPressedName(string name, string typed)
+    {
+        int i = 0, j = 0, n = name.size(), t = typed.size();
+        while (j < t)
+        {
+            // If characters match in both strings
+            if (name[i] == typed[j])
+            {
+                i++;
+                j++;
+            }
+            // Skip repeated character
+            else if (j > 0 && typed[j] == typed[j - 1])
+                j++;
+            // Mismatched characters
+            else
+                return false;
+        }
+        // All characters in name are checked
+        return i == n;
+    }
+};
