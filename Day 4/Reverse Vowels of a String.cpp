e136519a40b40d2c4273e53e6fb3915d8648e9f5
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-05 09:33:07 +0500

    Reverse Vowels of a String

---
+++ Day 4/Reverse Vowels of a String.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    bool isVowel(char c)
+    {
+        if ((c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ||
+            (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'))
+            return true;
+        return false;
+    }
+
+public:
+    string reverseVowels(string s)
+    {
+        int i = 0, j = s.size() - 1;
+        while (i <= j)
+        {
+            // Iterate until a vowel is not found at start
+            if (!isVowel(s[i]))
+                i++;
+            // Iterate until a vowel is not found at end
+            else if (!isVowel(s[j]))
+                j--;
+            // When vowels are found at both places, swap them
+            else
+                swap(s[i], s[j]), i++, j--;
+        }
+        return s;
+    }
+};
