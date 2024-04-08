5fee3230f5fff39d53989785b8a2c551d0bfdde0
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-08 20:40:15 +0500

    Faulty Keyboard

--- Month 1/Day 2/Faulty Keyboard.cpp
+++ Month 1/Day 2/Faulty Keyboard.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string finalString(string s) {
         string ans;
-        for(int i = 0; i < s.size(); i++)
+        for(int i = 0; i < s.size(); i++) 
         {
             // If found 'i' reverse the string otherwise add to answer
             if(s[i] != 'i')
