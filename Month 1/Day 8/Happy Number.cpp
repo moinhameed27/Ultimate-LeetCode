1d285fd3543b4c0c479c20eeca40026fcff6020b
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-09 09:31:38 +0500

    Happy Number

---
+++ Month 1/Day 8/Happy Number.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    bool isHappy(int n)
+    {
+        while (n > 9)
+        {
+            int sum = 0;
+            int dup = n;
+            while (dup > 0)
+            {
+                int div = dup % 10;
+                sum += div * div;
+                dup /= 10;
+            }
+            n = sum;
+        }
+        // 1 and 7 are the only two single digit happy numbers 
+        return n == 1 || n == 7;
+    }
+};
