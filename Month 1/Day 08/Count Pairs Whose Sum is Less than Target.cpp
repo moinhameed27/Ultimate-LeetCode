4d2237cbb8ac287d0aeb114923a99a3a888e2a0c
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-11 10:11:51 +0500

    Count Pairs Whose Sum is Less than Target

--- Month 1/Day 08/Count Pairs Whose Sum is Less than Target.cpp
+++ Month 1/Day 08/Count Pairs Whose Sum is Less than Target.cpp
@@ -3,7 +3,7 @@ class Solution
 public:
     int countPairs(vector<int> &nums, int target)
     {
-        sort(begin(nums), end(nums));
+        sort(begin(nums), end(nums)); 
         int count = 0, n = nums.size();
         int i = 0, j = n - 1;
         while (i < j)
