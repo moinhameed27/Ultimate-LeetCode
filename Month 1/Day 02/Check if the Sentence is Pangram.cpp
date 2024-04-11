9562ca0add4dda526ab82e4872b8a26a5e2c83a7
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-11 10:07:32 +0500

    Check if the Sentence is Pangram

--- Month 1/Day 02/Check if the Sentence is Pangram.cpp
+++ Month 1/Day 02/Check if the Sentence is Pangram.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkIfPangram(string sentence) {
         vector<int> freq(26, 0);
-        // Storing the frequency of each character
+        // Storing the frequency of each character 
         for(int i = 0; i < sentence.size(); i++)
             freq[sentence[i] - 'a']++; 
         
