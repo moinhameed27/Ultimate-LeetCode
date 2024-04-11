5e39d75edf58ea309ec4bbf1d12f98a108a3bb87
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-11 10:08:00 +0500

    Faulty Keyboard

--- Month 1/Day 02/Faulty Keyboard.cpp
+++ Month 1/Day 02/Faulty Keyboard.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i < s.size(); i++) 
         {
             // If found 'i' reverse the string otherwise add to answer
-            if(s[i] != 'i')
+            if(s[i] != 'i') 
                 ans += s[i];
             else
                 reverse(ans.begin(), ans.end());
