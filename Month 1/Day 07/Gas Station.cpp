1f2c875613b6d0fc211a24d644686a4f02fe5d61
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-11 10:11:20 +0500

    Gas Station

--- Month 1/Day 07/Gas Station.cpp
+++ Month 1/Day 07/Gas Station.cpp
@@ -3,7 +3,7 @@ class Solution
 public:
     int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
     {
-        int n = gas.size();
+        int n = gas.size(); 
         int start = 0, tank = 0, total = 0;
 
         for (int i = 0; i < n; ++i)
